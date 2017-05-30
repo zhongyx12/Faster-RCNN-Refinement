d0809ed1659bfaa6d066d495e858d673afa10e6f
zhongyx12 <zhongyx4869@163.com>
2017-05-30 01:45:02 -0700

    debug

--- faster_rcnn/faster_rcnn.py
+++ faster_rcnn/faster_rcnn.py
@@ -248,10 +248,9 @@ class FasterRCNN(nn.Module):
                 x = self.fc7(x)
                 x = F.dropout(x, training=self.training)
                 x = torch.unsqueeze(x, 0)
-                print(rois.size(0))
                 if it == 0:
-                    prev_h = Variable(torch.zeros(1, x.size(1), 4096), requires_grad=False).cuda()
-                    prev_c = Variable(torch.zeros(1, x.size(1), 4096), requires_grad=False).cuda()
+                    prev_h = Variable(torch.zeros(1, rois.size(0), 4096), requires_grad=False).cuda()
+                    prev_c = Variable(torch.zeros(1, rois.size(0), 4096), requires_grad=False).cuda()
                     hiddens = (prev_h, prev_c)
                 x, hiddens = self.lstm(x, hiddens)
                 x = torch.squeeze(x, 0)
